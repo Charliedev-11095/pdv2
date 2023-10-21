65843c79f3657b81869785d1e5b0aa1d572dade7
Charliedev-11095 <carlosroyiga@gmail.com>
2023-10-21 00:01:25 -0600

    ohh si

--- accounts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import User, DetallesUsuario, Domicilio
-
-admin.register(User, DetallesUsuario, Domicilio)(admin.ModelAdmin)
--- cuentas/models.py
+++ cuentas/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-class User(models.Model):
-    username = models.CharField(max_length=100)
-
-
 class DetallesUsuario(models.Model):
     GENDER_CHOICES = [
         ('masculino', 'Masculino'),
