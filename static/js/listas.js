ede9673b711e35060cc587b8b4763c14150aacf9
Charliedev-11095 <carlosroyiga@gmail.com>
2023-10-29 09:54:43 -0600

    falta vistas crud usuarios

---
+++ static/js/listas.js
@@ -0,0 +1,30 @@
+
+            $(document).ready(function() {
+                $('#example').DataTable({
+                    "ajax": {
+                        "url": "/accounts/listaUsuarios/",
+                        "dataSrc": ""  // Deja esto en blanco para que los datos se tomen directamente del JSON
+                    },
+                    "columns": [
+                        { "data": "usuario_id" },
+                        { "data": "Nombre" },
+                        { "data": "Apellido Paterno" },
+                        { "data": "Apellido Materno" },
+                        { "data": "Género" },
+                        { "data": "Fecha de Nacimiento" },
+                        { "data": "Correo Electrónico" },
+                        { "data": "Teléfono" },
+                        { "data": "Rol" },
+                        { "data": "RFC" },
+                        { "data": "Razón Social" },
+                        { "data": "Calle" },
+                        { "data": "Ciudad" },
+                        { "data": "Estado" },
+                        { "data": "País" },
+                        {
+                            "data": null,
+                            "defaultContent": "<button>Edit</button> <button>Delete</button>"
+                        }
+                    ]
+                });
+            });
