aa0559e4da3f2f6a92337cabaf0e1829c5bab5db
Charliedev-11095 <carlosroyiga@gmail.com>
2023-10-30 20:42:45 -0600

    respaldo

--- punto_venta_django2/urls.py
+++ punto_venta_django2/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),  # new
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/login/", TemplateView.as_view(template_name="registration/login.html"), name="home"),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- static/js/listas.js
+++ static/js/listas.js
@@ -1,33 +1,31 @@
-
-            $(document).ready(function() {
-                $('#example').DataTable({
-                    "ajax": {
-                        "url": "/accounts/listaUsuarios/",
-                        "dataSrc": ""  // Deja esto en blanco para que los datos se tomen directamente del JSON
-                    },
-                    "columns": [
-                        { "data": "usuario_id" },
-                        { "data": "Nombre" },
-                        { "data": "Apellido Paterno" },
-                        { "data": "Apellido Materno" },
-                        { "data": "Género" },
-                        { "data": "Fecha de Nacimiento" },
-                        { "data": "Status" },
-                        { "data": "Rol" },
-                        { "data": "Calle" },
-                        { "data": "Ciudad" },
-                        { "data": "Estado" },
-                        { "data": "País" },
-                        { "data": "Teléfono" },
-                        { "data": "Celular" },
-                        { "data": "Correo Electrónico" },
-                        { "data": "RFC" },
-                        { "data": "Razón Social" },
-                        
-                        {
-                            "data": null,
-                            "defaultContent": "<button>Edit</button> <button>Delete</button>"
-                        }
-                    ]
-                });
-            });
+$(document).ready(function() {
+    $('#example').DataTable({
+        "ajax": {
+            "url": "/accounts/listaUsuarios/",
+            "dataSrc": ""  // Deja esto en blanco para que los datos se tomen directamente del JSON
+        },
+        "columns": [
+            { "data": "usuario_id" },
+            { "data": "Nombre" },
+            { "data": "Apellido Paterno" },
+            { "data": "Apellido Materno" },
+            { "data": "Género" },
+            { "data": "Fecha de Nacimiento" },
+            { "data": "Status" },
+            { "data": "Rol" },
+            { "data": "Calle" },
+            { "data": "Ciudad" },
+            { "data": "Estado" },
+            { "data": "País" },
+            { "data": "Teléfono" },
+            { "data": "Celular" },
+            { "data": "Correo Electrónico" },
+            { "data": "RFC" },
+            { "data": "Razón Social" },
+            {
+                "data": null,
+                "defaultContent": "<button class='btn btn-primary mr-2'>Editar</button> <button class='btn btn-danger'>Borrar</button>"
+            }
+        ]
+    });
+});
